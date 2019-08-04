b41e0a9b8c099a10129fcd366a693d136e8f6140
Derya Taşdöğen <deryatasdogen@deryas-mbp.lan>
2019-08-04 16:05:24 +0200

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayComponent } from './components/display/display.component';
+import { OperationComponent } from './components/operation/operation.component';
+import { ButtonComponent } from './components/button/button.component';
+import { NumberpadComponent } from './components/numberpad/numberpad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayComponent,
+    OperationComponent,
+    ButtonComponent,
+    NumberpadComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/numberpad/numberpad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-numberpad',
+  templateUrl: './numberpad.component.html',
+  styleUrls: ['./numberpad.component.scss']
+})
+export class NumberpadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
